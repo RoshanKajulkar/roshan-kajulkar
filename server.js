ed63528decee4c4421cd0940d24a4527c3c76401
Roshan Kajulkar <roshankajulkar@gmail.com>
2025-06-29 14:35:00 +0530

    update landing message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Building my portfolio. PLease check back later. :)");
 });
 
 let lastApiHit = "";
