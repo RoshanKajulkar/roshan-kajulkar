7d2e1f38e877d06b05b20305264f846321a24e91
Roshan Kajulkar <roshankajulkar@gmail.com>
2025-06-29 12:54:43 +0530

    keep alive

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 import express from "express";
+import axios from "axios";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+let lastApiHit = "";
+
+const hitApi = async () => {
+  try {
+    const endpoint = "https://roshan-kajulkar.onrender.com/";
+    await axios.get(endpoint);
+    lastApiHit = new Date().toLocaleString("en-IN", {
+      timeZone: "Asia/Kolkata",
+    });
+  } catch (error) {
+    logger.error(`Error making API call to ${endpoint}`);
+  }
+};
+
+setInterval(hitApi, 1 * 60 * 1000);
+
+hitApi();
+
+app.get("/ping", (req, res) => {
+  res.send(
+    `"https://roshan-kajulkar.onrender.com/" was last pinged on - ${lastApiHit}`
+  );
 });
 
 app.listen(port, () => {
